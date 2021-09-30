2c3fd0323bf53d7dcbad9412201fdde4e9a4d029
wangxiaowei14227 <wangxiaowei14227@autohome.com.cn>
2021-09-30 18:53:20 +0800

    demo

--- reactor-samples-core/src/test/java/wangxw/operator/ErrorHandleOperatorTest.java
+++ reactor-samples-core/src/test/java/wangxw/operator/ErrorHandleOperatorTest.java
@@ -1,11 +1,18 @@
 package wangxw.operator;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import reactor.core.Disposable;
+import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.SignalType;
+import reactor.util.retry.Retry;
 
+import java.time.Duration;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Author: wangxw
@@ -14,7 +21,30 @@ import java.util.Objects;
  */
 @Slf4j
 public class ErrorHandleOperatorTest {
+    AtomicBoolean isDisposed = new AtomicBoolean();
+    Disposable disposableInstance = new Disposable() {
+        @Override
+        public void dispose() {
+            isDisposed.set(true);
+        }
 
+        @Override
+        public String toString() {
+            return "DISPOSABLE";
+        }
+    };
+
+    @Test
+    public void using() {
+        Flux<String> flux =
+                Flux.using(
+                        () -> disposableInstance, // 创建资源
+                        disposable -> Flux.just(disposable.toString()), // 处理资源
+                        Disposable::dispose // 清理资源
+                );
+
+        flux.subscribe(log::info);
+    }
 
     @Test
     public void staticValueFallback() {
@@ -66,6 +96,73 @@ public class ErrorHandleOperatorTest {
 
                 });
         flux.subscribe(log::info, e -> log.error("err", e));
+    }
+
+
+    @SneakyThrows
+    @Test
+    public void retryTest() {
+        Flux.interval(Duration.ofMillis(250))
+                .map(input -> {
+                    if (input < 3) return "tick " + input;
+                    throw new RuntimeException("boom");
+                })
+                .retry(1)
+                .elapsed()
+                .subscribe(res -> log.info(res.toString()), e -> log.error("error", e));
+
+        Thread.sleep(2100);
+    }
+
+    @Test
+    @SneakyThrows
+    public void restryWhen() {
+        Flux.interval(Duration.ofMillis(250))
+                .map(input -> {
+                    if (input < 3) return "tick " + input;
+                    throw new RuntimeException("boom");
+                })
+                .doOnError(e -> log.error("on error", e))
+                .retryWhen(Retry.from(companion ->
+                        companion.doOnNext(retrySignal -> log.info("retry times {} ", retrySignal.totalRetries())).
+                                take(3)))
+                .retry(3)
+                .elapsed()
+                .subscribe(res -> log.info(res.toString()), e -> log.error("error", e));
+        Thread.sleep(50000);
+    }
+
+    @Test
+    @SneakyThrows
+    public void restryWhen1() {
+        Flux.<String>error( new RuntimeException("boom"))
+                .doOnError(e -> System.err.println("on error"))
+//                .retryWhen(Retry.from(companion ->
+//                        companion
+////                                .doOnNext(retrySignal -> log.info("retry times {} ", retrySignal.totalRetries()))
+//                                .take(3)))
+                .retry(3)
+                .subscribe(System.out::println, System.err::println);
+    }
+
+
+    @Test
+    @SneakyThrows
+    public void restryWhen2() {
+        Flux.<String>error(new IllegalArgumentException())
+                .doOnError(e -> log.error("on error", e))
+                .retryWhen(Retry.from(companion ->
+                        companion.map(rs -> {
+                            if (rs.totalRetries() < 3) {
+                                return rs.totalRetries();
+                            } else {
+                                throw Exceptions.propagate(rs.failure());
+                            }
+                        }).doOnNext(times -> log.info("retry times {} ", times))
+                ))
+                .subscribe(log::info, throwable -> log.error("throwable", throwable));
+        Thread.sleep(50000);
+
     }
 
     public String doSomethingDangerous(int i) {
